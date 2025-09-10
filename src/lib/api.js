11ec49a0a7ddf90644989a3b02caf6eab6ebeacd
unknown <yehoshaphatij@gmail.com>
2025-09-10 16:55:21 +0300

    Twist variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'process.env.REACT_APP_URL' ||'http://localhost:4000',
+  baseURL: 'import.meta.env.VITE_BACKEND_URL' ||'http://localhost:4000',
    withCredentials: true,
   //headers: { 'ngrok-skip-browser-warning': '1' }
 });
