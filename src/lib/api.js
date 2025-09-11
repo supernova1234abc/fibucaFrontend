eff19078f2955f5a0a8def4c22cc0948eba1cb95
unknown <yehoshaphatij@gmail.com>
2025-09-11 10:40:47 +0300

    update BACKEND_URL

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'import.meta.env.VITE_BACKEND_URL' ||'http://localhost:4000',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
    withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
 });
