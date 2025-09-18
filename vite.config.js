e20ac60b409559e99dae25cd493cdc526d290421
unknown <yehoshaphatij@gmail.com>
2025-09-18 15:45:18 +0300

    fix last new 5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, allowedHosts: 'all' },
-  build: { outDir: 'dist' }
+  base: '/', // important for Vercel routing
+  build: {
+    outDir: 'dist', // default, just make sure
+  },
 });
