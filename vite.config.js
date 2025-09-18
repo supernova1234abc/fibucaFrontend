67a2ba2b79a307b374541e42dc888f7a11955c68
unknown <yehoshaphatij@gmail.com>
2025-09-18 14:47:18 +0300

    fix 01

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: { host: true, allowedHosts: 'all' },
   build: { outDir: 'dist' }
 });
