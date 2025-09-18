2ee337c6660e85b37361eb071b6f1d258672cae9
unknown <yehoshaphatij@gmail.com>
2025-09-18 09:20:30 +0300

    fix again  issues2

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-
+  base: '/',
+  plugins: [react()],
   server: {
-    host: true, // 👈 allows access from LAN or public IP
-    allowedHosts: true, // 👈 allows any hostname (including Ngrok URLs)
-  },
-
-    plugins: [react()],
-    base:"/",
-})
+    host: true,
+    allowedHosts: 'all'
+  }
+});
