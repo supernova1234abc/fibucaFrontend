8e75fcdf027254ca634950f3a9a509ceeae5fd45
supernova <yehoshaphatij@gmail.com>
2026-03-06 11:10:04 +0300

    try well5

--- src/components/IDCard.jsx
+++ src/components/IDCard.jsx
@@ -203,7 +203,7 @@ const IDCard = forwardRef(({ card }, ref) => {
           {/* FRONT */}
           <div ref={frontRef} className={cardStyle}>
             {/* PHOTO LAYER */}
-            <div className="absolute top-10 left-4 w-[104px] z-10">
+            <div className="absolute top-10 left-0 w-[104px] z-10">
               <div className="relative w-[104px] h-[104px] overflow-hidden rounded-full bg-transparent">
                 {photoSrc ? (
                   <>
