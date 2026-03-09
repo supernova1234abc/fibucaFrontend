2f26606df8d1e99dfe33be31618332fab90cd0e8
supernova <yehoshaphatij@gmail.com>
2026-03-09 21:49:34 +0300

    change logo watermark11 now fixed

--- src/components/IDCard.jsx
+++ src/components/IDCard.jsx
@@ -244,7 +244,7 @@ const IDCard = forwardRef(({ card }, ref) => {
             </div>
 
             {/* watermark/logo */}
-            <div className="absolute inset-0 flex items-center justify-center opacity-50 pointer-events-none z-20">
+            <div className="absolute inset-0 flex items-center justify-center opacity-40 pointer-events-none z-20">
               <img
                 src="/images/logo-watermark.png"
                 alt="Watermark"
